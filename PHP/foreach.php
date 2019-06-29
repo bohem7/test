d3341b9face34ad6ee293d0f02915467b7b4906e
bohem7 <bohem7@prontonmail.com>
2019-06-29 23:09:27 +0800

    19.06.29

---
+++ PHP/foreach.php
@@ -0,0 +1,11 @@
+<?php
+	$arr1 = [1,2,3,4,5,6,7];
+	$arr2 = array(7,6,5,4,3,2,1);
+	$arr3 = ['0'=>1,'1'=>2,'2'=>3,'3'=>4];
+	
+	foreach($arr1 as $i){
+		var_dump($i);
+		echo PHP_EOL;
+		echo $i;
+	}
+?>
